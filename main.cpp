f75a1b2c8432589d5c59127f6dee13316e9a4231
fojoWork <foxyovich@gmail.com>
2026-07-14 00:33:50 -0700

    Optimized for better performance

--- main.cpp
+++ main.cpp
@@ -1,42 +1,69 @@
-#include <chrono>
+#include <iostream>
+#include <cstdlib>
 #include <conio.h>
-#include <windows.h>
-#include "classes.hpp"
-#include <thread>
-#include <windows.h>
+#include <ctime>
 
-void hideConsoleCursor() {
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-    
-    GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = false; 
-    SetConsoleCursorInfo(out, &cursorInfo);
+
+struct Food {
+  int x;
+  int y;
+};
+
+void generate_food_spawn(Food &food) {
+  food.x = rand() % 5;
+  food.y = rand() % 5;
 }
 
-extern int points;
+void display_grid(int x, int y, Food &food) {
+  std::cout << "\033[H\033[2J"; 
+  
+  for (size_t i{}; i < 5; i++) {
+    for (size_t j{}; j < 5; j++) {
+      if (j == x && i == y) {
+        std::cout << "+ ";
+      }
+      else if (j == food.x && i == food.y) {
+        std::cout << "* ";
+      }
+      else {
+        std::cout << ". ";
+      }
+    }
+    std::cout << "\n";
+  }
+}
 
 int main() {
-    bool isactive = true;
-    Movement movement;
-    Food food;
-    Map game;
-    
-    while(isactive) {
-        system("cls");
-        hideConsoleCursor();
-        game.display_grid(food, movement);
-        
-        if(_kbhit()) {
-            char key = _getch();
-            if (key == 'w') movement.move_up();
-            if (key == 's') movement.move_down();
-            if (key == 'a') movement.move_left();
-            if (key == 'd') movement.move_right();
-            if (key == 'q') return 0; 
-        }
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  srand(time(NULL));
+  
+  Food food = {0, 0};
+  generate_food_spawn(food);
+  
+  bool active = true;
+  int player_x = 2, player_y = 2;
+
+
+  void (*display)(int, int, Food&) = display_grid;
+
+  display(player_x, player_y, food);
+
+  while(active) {
+    if(_kbhit()) {
+      char key = _getch();
+
+      if(key == 'w' && player_y > 0) player_y--;
+      if(key == 's' && player_y < 4) player_y++;
+      if(key == 'a' && player_x > 0) player_x--;
+      if(key == 'd' && player_x < 4) player_x++;
+      if(key == 'q') active = false;
+
+      // 3. Check if player stepped on the food!
+      if (player_x == food.x && player_y == food.y) {
+          generate_food_spawn(food);
+      }
+
+      display(player_x, player_y, food);
     }
-    return 0;
+  }
+  return 0;
 }
